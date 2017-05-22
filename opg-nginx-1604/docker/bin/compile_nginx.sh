c0f8df5a16abf04cce22ef935e944b10a469103b
Brett Minnie <github@bdb-studios.co.uk>
2017-05-22 12:51:59 +0100

    Added nginx image

---
+++ opg-nginx-1604/docker/bin/compile_nginx.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+apt install -y libpcre3-dev zlib1g-dev libssl-dev libossp-uuid-dev uuid build-essential
+
+NGINX_VERSION="1.11.13"
+
+useradd -r nginx
+mkdir -p /var/lib/nginx && chown nginx:nginx /var/lib/nginx
+mkdir -p /var/log/nginx && chown nginx:nginx /var/log/nginx
+cd  /tmp
+wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
+tar -xzf nginx-${NGINX_VERSION}.tar.gz
+cd nginx-${NGINX_VERSION}
+./configure \
+  --user=nginx \
+  --group=nginx \
+  --prefix=/etc/nginx \
+  --conf-path=/etc/nginx/nginx.conf \
+  --sbin-path=/usr/sbin/nginx \
+  --error-log-path=/var/log/nginx/error.log \
+  --http-client-body-temp-path=/var/lib/nginx/body \
+  --http-fastcgi-temp-path=/var/lib/nginx/fastcgi \
+  --http-log-path=/var/log/nginx/access.log \
+  --http-proxy-temp-path=/var/lib/nginx/proxy \
+  --lock-path=/var/lock/nginx.lock \
+  --pid-path=/var/run/nginx.pid \
+  --with-http_gzip_static_module \
+  --with-http_stub_status_module \
+  --with-http_ssl_module \
+  --with-pcre-jit
+make
+make install
+
+rm /tmp/nginx-${NGINX_VERSION}.tar.gz
+rm -rf /tmp/nginx-*
+
+apt remove -y --purge build-essential
+apt clean
+apt autoremove -y
+rm -rf /var/lib/cache/* /var/lib/log/* /tmp/* /var/tmp/*
