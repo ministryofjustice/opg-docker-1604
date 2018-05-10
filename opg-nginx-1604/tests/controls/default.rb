09b9f3653988217bc774e448ef6b3699ecd86218
Dan Webb <dan.webb@damacus.io>
2018-05-10 14:17:41 +0100

    Add incpsec tests Reduce the number of layers in the Dockerfile - Single COPY comand, copy using a directory structure - Collapse the ENV commands together
    Move Bash tests to inspec and actually run them.

---
+++ opg-nginx-1604/tests/controls/default.rb
@@ -0,0 +1,29 @@
+# encoding: utf-8
+
+title 'Program Installed'
+
+control 'Services' do
+  impact 1
+  title 'nginx service'
+  desc 'nginx service is setup and installed'
+  describe runit_service('nginx') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+end
+
+# Test is performed from within the Container
+# so that we don't need to port forward or try and avoid port conflicts.
+# This is acceptable in this case as we're testing Nginx not the docker-compose
+# setup.
+control 'Nginx Configured Correctly' do
+  impact 1
+  title 'Nginx Configured'
+  desc 'Nginx should be configured correctly'
+  describe http('https://localhost', ssl_verify: false, enable_remote_worker: true) do
+    its('status') { should cmp 200 }
+    its('body') { should match 'Hello World from opguk/nginx' }
+    its('headers.Content-Type') { should cmp 'text/html' }
+    its('headers.X-Request-Id') { exist }
+  end
+end
