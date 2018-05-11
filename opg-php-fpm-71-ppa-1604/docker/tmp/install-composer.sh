1125500a5aca86b26d3458d42df2385656d17299
Ali Bahman <abn@webit4.me>
2018-05-11 13:24:40 +0100

    OPGOPS-2732 Shift the composer installer script to the end of the run

--- opg-php-fpm-71-ppa-1604/docker/tmp/install-composer.sh
+++ opg-php-fpm-71-ppa-1604/docker/tmp/install-composer.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -ex
+
 EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
@@ -12,6 +14,6 @@ then
 fi
 
 php composer-setup.php --install-dir=/usr/bin --filename=composer
-RESULT=$?
+#RESULT=$?
 rm composer-setup.php
-exit $RESULT
+#exit $RESULT
